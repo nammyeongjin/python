235195c833ddb23215327db2d26c7bb1ecce8833
nammyeongjin <jinnammyeong@gmail.com>
2025-09-01 12:50:22 +0900

    파이썬4일차

---
+++ index.py
@@ -0,0 +1,13 @@
+#문자열 인덱싱하기
+a = 'life is too short, You need python'
+print(len(a))
+print(a[5])
+print(a[19])
+print(a[0]+a[19]+a[28])
+print(a[12:17])
+print(a[8:11])
+print(a[28:35])
+print(a[19:])
+print(a[:17])
+print(a[19:-7])
+print(a[0:-17])
