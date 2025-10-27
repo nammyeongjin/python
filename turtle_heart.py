f800fd85f30247afef39e1956240b58704313c3f
nammyeongjin <jinnammyeong@gmail.com>
2025-10-27 12:49:36 +0900

    파이썬 거북이 하트

---
+++ turtle_heart.py
@@ -0,0 +1,24 @@
+#turtle_heart.py
+import turtle
+turtle.bgcolor('black')
+turtle.pensize(5)
+
+def curve():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(1)
+
+
+turtle.speed(0)
+turtle.color('red','magenta')
+
+turtle.begin_fill()
+turtle.left(140)
+turtle.forward(111)
+curve()
+
+turtle.left(120)
+curve()
+turtle.forward(111)
+turtle.end_fill()
+turtle.hideturtle()
